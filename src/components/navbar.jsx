d21a9a3ca505f74770d3d2ce7554345d98d74d0d
shilpashingnapure <shingnapureshilpa2@gmail.com>
2022-12-24 15:46:43 +0530

    do

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,12 +4,17 @@ import { useSelector } from "react-redux"
 export const Navbar = ()=>{
     const user = useSelector((s)=> s)
     return <div className="navbar">
-        {user ? <>
+        <div>
+        <img src='https://aniportalimages.s3.amazonaws.com/media/details/Capture2021092207014520210922071739.jpg'/>
+        </div>
+        <div>        {user ? <>
             <Link to='/tickets'>Home</Link>
             <Link to='/bookmark'>Bookmark</Link>
         </>: <>
             <Link to='/login'>Sign up</Link>
         </>}
+        </div>
+
 
     </div>
 }
